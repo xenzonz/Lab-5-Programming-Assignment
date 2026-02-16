110169e53388fb17a66534b48fb6ea01ff00c3a7
sam cocquyt <samcocquyt04@gmail.com>
2026-02-15 22:38:04 -0500

    docstring added

--- Lab5_xenzonz-1.py
+++ Lab5_xenzonz-1.py
@@ -1,1 +1,11 @@
-print(" hello world")
+"""
+Docstring for Lab5_xenzonz_1
+i. LAB 5: Dice Rolling Terms
+ii. Sam Cocquyt
+iii. Program that rolls two dice, prints the outcome, and then uses conditional statements to print the appropriate term for that roll
+iv. No starter code
+v. 2/15/2026
+"""
+
+import random
+
