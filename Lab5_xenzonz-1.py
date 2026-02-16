35e2573ae4492fbd86226021dace4b647d90e20e
sam cocquyt <samcocquyt04@gmail.com>
2026-02-15 22:44:00 -0500

    dice 1 and dice 2 added

--- Lab5_xenzonz-1.py
+++ Lab5_xenzonz-1.py
@@ -2,13 +2,15 @@
 Docstring for Lab5_xenzonz_1
 i. LAB 5: Dice Rolling Terms
 ii. Sam Cocquyt
-iii. Program that rolls two dice, prints the outcome, and then uses conditional statements to print the appropriate term for that roll
+iii. Program that rolls two dice, prints the outcome, and then uses conditional statements to print the appropriate term for that roll.
+    Repeats until the user quits
 iv. No starter code
 v. 2/15/2026
 """
 
 import random
 
-dice_roll = random.randint(1, 6)
+dice_one = random.randint(1, 6)
+dice_two = random.randint(1, 6)
 
-print(dice_roll)
+print(dice_one, dice_two)
