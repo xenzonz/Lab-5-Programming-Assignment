23a9d6f2426f8dddf4ba9848c035611693eea11c
sam cocquyt <samcocquyt04@gmail.com>
2026-02-15 23:03:02 -0500

    further added get_dice_term logic

--- Lab5_xenzonz-1.py
+++ Lab5_xenzonz-1.py
@@ -11,20 +11,27 @@ v. 2/15/2026
 import random
 
 def get_dice_term(dice_one, dice_two):
-    total = dice_one + dice_two
+    #total = dice_one + dice_two
 
-    print(total) #test
+    #print(total) #test
 
     if dice_one == 1 and dice_two == 1: 
         return "Snake Eyes"
+    elif dice_one == 2 and dice_two == 1:
+        return "Ace Caught a Deuce"
+    elif dice_one == 2 and dice_two == 2:
+        return "Little Joe from Kokomo"
+    elif dice_one == 3 and dice_two == 2:
+        return "Little Phoebe"
+    
 
 
 
 def main():
     print("test")
     
-    dice_one = random.randint(1, 1) #TTEST RETURN TO 1, 6
-    dice_two = random.randint(1, 1)
+    dice_one = random.randint(2, 3) #TTEST RETURN TO 1, 6
+    dice_two = random.randint(2, 3)
     total = dice_one + dice_two
 
     print(f"dice one: {dice_one}")
